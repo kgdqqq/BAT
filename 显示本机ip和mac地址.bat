0b7da0fa4ec998f639e8ed66cf783b9f815cefc9
阿童木 <winpe@msn.cn>
2021-04-27 13:46:15 +0800

    Update 显示本机ip和mac地址.bat

--- 显示本机ip和mac地址.bat
+++ 显示本机ip和mac地址.bat
@@ -1,7 +1,6 @@
 @echo off
-
-C:\Windows\System32\ipconfig /all |C:\Windows\System32\FIND.EXE "IPv4 地址" > d:\ip.log
-C:\Windows\System32\ipconfig /all |C:\Windows\System32\FIND.EXE "物理地址." > d:\mac.log
+C:\Windows\System32\ipconfig /all |C:\Windows\System32\FIND.EXE "IPv4 鍦板潃" > d:\ip.log
+C:\Windows\System32\ipconfig /all |C:\Windows\System32\FIND.EXE "鐗╃悊鍦板潃." > d:\mac.log
 for /f "tokens=6" %%b in (d:\ip.log) do (    set a=%%b)
 set  get=%a:~0,1%
 echo %get%
